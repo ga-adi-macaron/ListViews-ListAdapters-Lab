f0a559021e673da18fedaefa39cb4b5cef61620e
cbrads61 <cbrads61@gmail.com>
2016-10-17 19:50:53 -0400

    Colin Bradley ListViews and ListAdapters Lab

--- starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
+++ starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
@@ -1,11 +1,13 @@
 package ly.generalassemb.drewmahrt.bookshelf;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -20,13 +22,14 @@ public class MainActivity extends AppCompatActivity {
     BaseAdapter mBookAdapter;
 
     //TODO: Define your ListView
-
+    ListView mListView;
     //TODO: Define your Book List
-
+    List<Book> mBookList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //Use helper method to add books to the list
         mBookList = generateBooks();
@@ -52,10 +55,35 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 //TODO: Update the view
+                if (convertView==null){
+                    LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+                    convertView = layoutInflater.inflate(android.R.layout.simple_list_item_2, null);
+                }
+                TextView text1 = (TextView) convertView.findViewById(android.R.id.text1);
+                TextView text2 = (TextView) convertView.findViewById(android.R.id.text2);
+                text1.setText(mBookList.get(position).getTitle());
+                text2.setText(mBookList.get(position).getAuthor());
+                return convertView;
             }
         };
 
         //TODO: Set the ListView's adapter
+        mListView = (ListView)findViewById(R.id.listView);
+        mListView.setAdapter(mBookAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                text1.setTextColor(Color.RED);
+                TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+                text2.setTextColor(Color.RED);
+            }
+        });
+
+
+
+
 
     }
 
