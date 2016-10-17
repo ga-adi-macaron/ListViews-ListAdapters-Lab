79be7b2891c321145d7336b286872ba8225b209d
NikShuvalov <nshuvalov89@gmail.com>
2016-10-17 19:53:57 -0400

    Finished requirement

---
+++ MyBookShelf/app/src/main/java/shuvalov/nikita/mybookshelf/Book.java
@@ -0,0 +1,31 @@
+package shuvalov.nikita.mybookshelf;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by drewmahrt on 12/16/15.
+ */
+public class Book {
+    private String mTitle;
+    private String mAuthor;
+    private int mDate;
+    private int mImg;
+    public Book(String title,String author, int date, int img){
+        mTitle = title;
+        mAuthor = author;
+        mDate = date;
+        mImg = img;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getDate(){return String.valueOf(mDate);}
+
+    public int getImgPath(){return mImg;}
+}
